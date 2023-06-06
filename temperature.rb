28c43b50da0bcdb8fd478525c5f4e009a49187cd
Nao1013 <121780890+Nao1013@users.noreply.github.com>
2023-06-06 13:13:28 +0000

    practice

--- temperature.rb
+++ temperature.rb
@@ -8,16 +8,17 @@
 
 room = gets.to_i
 air = gets.to_i
+windy = gets.to_i
 
 diff = (room - air).abs
 
 temperature = 0
 if diff < 5
-  puts "15"
+  temperature = 15
 elsif diff >= 5 && diff < 10
- puts "30"
+  temperature = 30
 elsif diff >= 10
-  puts "60"
+  temperature = 60
 end
 
 # 風量によって設定温度になるまでの時間に変化を作る
@@ -25,7 +26,6 @@ end
 # 　・1:低
 # 　・2:中
 # 　・3:大
-　
 # 　・風量1
 # 　・デフォルト
 # ・風量2
@@ -34,3 +34,15 @@ end
 # 　・設定温度になるまでが10分早くなる
 # ・1~3以外の入力の場合には"風量の値は1~3にしてください"と出力して、プログラムを終了
 
+if windy == 1
+  temperature
+elsif windy == 2
+  temperature = temperature - 5
+elsif windy == 3
+  temperature = temperature - 10
+else
+  puts "風量の値は1~3にしてください"
+  exit
+end
+
+p temperature
